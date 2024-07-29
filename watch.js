b412fe71955d7fb2e6196422d19a32b73a629746
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-07-29 15:54:26 +0700

    add file watch.js

---
+++ watch.js
@@ -0,0 +1,30 @@
+const chokidar = require('chokidar');
+const { exec } = require('child_process');
+
+const pathsToWatch = [
+    'styles/**/*.less',
+    'lib/pkp/styles/**/*.less',
+    'plugins/themes/default/styles/**/*.less'
+];
+
+const watcher = chokidar.watch(pathsToWatch, {
+    persistent: true,
+    ignoreInitial: true
+});
+
+const compileLess = () => {
+    exec('lessc styles/index.less public/journals/2/styleSheet.css', (err, stdout, stderr) => {
+        if (err) {
+            console.error(`Error: ${stderr}`);
+        } else {
+            console.log(`Compiled successfully: ${stdout}`);
+        }
+    });
+};
+
+watcher
+    .on('change', compileLess)
+    .on('add', compileLess)
+    .on('unlink', compileLess);
+
+console.log('Watching for changes...');
